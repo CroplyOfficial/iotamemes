68e42c75ecf072478cf5f3160738127bc460eb09
Coodos <info@coodos.co>
2021-06-15 15:05:50 +0530

    :tada: added /authenticate basic frontend logic

--- client/src/pages/AuthSuccess/AuthSuccess.tsx
+++ client/src/pages/AuthSuccess/AuthSuccess.tsx
@@ -1,9 +1,34 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import axios from 'axios';
 
-const AuthSuccess = ({ location }: any) => {
+const AuthSuccess = ({ location, history }: any) => {
   const query: string = location.search;
   const discordCode: string = query.split('code=')[1] || '';
+
+  const authorize = async () => {
+    const config = {
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    };
+
+    const { data } = await axios.post(
+      '/api/users/authenticate',
+      { code: discordCode },
+      config
+    );
+
+    return data;
+  };
+
+  useEffect(() => {
+    const auth = async () => {
+      await authorize();
+      history.push('/');
+    };
+
+    auth();
+  });
 
   return <div>{discordCode}</div>;
 };
