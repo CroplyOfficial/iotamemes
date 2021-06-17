6520b2d09e03c1018e15c4a9f6ba0a27ea83dee9
Coodos <info@coodos.co>
2021-06-17 15:13:26 +0530

    :tada: login works and token is stored in localstorage

---
+++ client/src/constants/userContants.ts
@@ -0,0 +1,4 @@
+export const USER_LOGIN_REQUEST: string = 'USER_LOGIN_REQUEST';
+export const USER_LOGIN_SUCCESS: string = 'USER_LOGIN_SUCCESS';
+export const USER_LOGIN_FAIL: string = 'USER_LOGIN_FAIL';
+export const USER_LOGOUT: string = 'USER_LOGOUT';
