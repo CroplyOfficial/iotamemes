604dfe95dd05772ed0eb5725d5dec5d31bfd5241
Robert Bornschein <robertbornschein@mailbox.tu-dreden.de>
2021-06-17 17:53:47 +0200

    ➕ Added bulma.css

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,7 +1,8 @@
-import './App.css';
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
-import AuthSuccess from './pages/AuthSuccess/AuthSuccess';
-import NewMeme from './pages/NewMeme/NewMeme';
+import "./App.css";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import AuthSuccess from "./pages/AuthSuccess/AuthSuccess";
+import NewMeme from "./pages/NewMeme/NewMeme";
+import { HomePage } from "./pages/Home/Home";
 
 function App() {
   return (
@@ -9,15 +10,16 @@ function App() {
       <Switch>
         <Route
           exact
-          path='/authorize'
+          path="/authorize"
           component={() => {
             window.location.href =
-              'https://discord.com/api/oauth2/authorize?client_id=854235929329795072&redirect_uri=http%3A%2F%2Flocalhost%3A3000%2Fauthenticate%2Fsuccess&response_type=code&scope=identify';
+              "https://discord.com/api/oauth2/authorize?client_id=854235929329795072&redirect_uri=http%3A%2F%2Flocalhost%3A3000%2Fauthenticate%2Fsuccess&response_type=code&scope=identify";
             return null;
           }}
         />
-        <Route exact path='/authenticate/success' component={AuthSuccess} />
-        <Route exact path='/newmeme' component={NewMeme} />
+        <Route exact path="/authenticate/success" component={AuthSuccess} />
+        <Route exact path="/newmeme" component={NewMeme} />
+        <Route exact path="/" render={() => <HomePage />} />
       </Switch>
     </Router>
   );
