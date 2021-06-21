3ce93f85b19d15961e81ca31cacddeb0d3e29c9f
Robert Bornschein <robertbornschein@mailbox.tu-dreden.de>
2021-06-21 18:42:52 +0200

    ⏳ added image, and boilerplate function

--- client/src/components/Memes/Modal/Modal.tsx
+++ client/src/components/Memes/Modal/Modal.tsx
@@ -1,3 +1,6 @@
+import {faShare} from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+
 interface IProps {
   meme: any;
   isActive: boolean;
@@ -5,20 +8,31 @@ interface IProps {
 }
 export const MemeModal = ({ meme, isActive, exitHandler }: IProps) => {
   const classNames = isActive ? "is-active" : "";
+  const {imgURL, memeAuthor,memeTags, upvotes, _id} = meme;
+
+  // Populate if you need it
+  const fetchAuthor = (_id: string) => {
+    return {};
+  }
+
+
+
   return (
     <div className={"modal " + classNames}>
       <div className="modal-background" onClick={exitHandler}></div>
       <div className="modal-card">
         <header className="modal-card-head">
-          <p className="modal-card-title">Modal title</p>
+          <p className="modal-card-title">{memeAuthor}</p>
           <button className="delete" aria-label="close" onClick={exitHandler}></button>
         </header>
         <section className="modal-card-body">
+          <div className="image " >
+            <img src={imgURL} />
+          </div>
           {/* <!-- Content ... --> */}
         </section>
         <footer className="modal-card-foot">
-          <button className="button is-success">Save changes</button>
-          <button className="button">Cancel</button>
+          <FontAwesomeIcon icon={faShare} />
         </footer>
       </div>
     </div>
